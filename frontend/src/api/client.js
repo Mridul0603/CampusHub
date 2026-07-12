a94e157a2eba2be7e6dc072458dc4d9bed596915
Mridul Tyagi <mridul.mt88@gmail.com>
2026-07-12 12:03:37 +0530

    Fix auth redirect loop

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -3,17 +3,9 @@ import axios from "axios";
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
   withCredentials: true,
-  headers: { "Content-Type": "application/json" },
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
-apiClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default apiClient;
